455d31cf3d8199e6be443a59624eb9970ffc1b51
Anto Martínez <antomartinezdev@gmail.com>
2024-04-17 12:37:32 +0200

    I modify thrower aim to stop some miliseconds when throw popcorn to get better aimming feeling

--- Assets/Scripts/Minigames/ThrowingPopcorns/ThrowerAim.cs
+++ Assets/Scripts/Minigames/ThrowingPopcorns/ThrowerAim.cs
@@ -5,11 +5,17 @@ public class ThrowerAim : MonoBehaviour{
     [SerializeField] float CircleRadius;
     [SerializeField] float RotationSpeed;
     [SerializeField] int AngularBounds;
-    
     private float m_CurrentAngle = 0f;
+    [Space(3)]
+    [SerializeField] float m_TimeStoppedWhenThrow = .3f;
+    private float m_CounterTimeStopped = 0f;
 
     void Update(){
-        CircularMove();
+        if(m_CounterTimeStopped <= 0){
+            CircularMove();
+        }else{
+            m_CounterTimeStopped -= Time.deltaTime;
+        }
     }
 
     void CircularMove(){
@@ -29,5 +35,9 @@ public class ThrowerAim : MonoBehaviour{
 
     bool OutInBounds(){
         return m_CurrentAngle > AngularBounds || m_CurrentAngle < -AngularBounds;
+    }
+
+    public void StopAimForThrow(){
+        m_CounterTimeStopped = m_TimeStoppedWhenThrow;
     }
 }
--- Assets/Scripts/Minigames/ThrowingPopcorns/ThrowerPlayerBehaviour.cs
+++ Assets/Scripts/Minigames/ThrowingPopcorns/ThrowerPlayerBehaviour.cs
@@ -1,5 +1,36 @@
 using UnityEngine;
 
 public class ThrowerPlayerBehaviour : MonoBehaviour{
-    
+    [SerializeField] Pool m_PopcornShots;
+    [SerializeField] ThrowerAim ThrowerAim;
+    private bool m_CanMove = false;
+
+    public void EnableMove(){
+        m_CanMove = true;
+    }
+
+    void Start(){
+        EnableMove();
+    }
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Space) && m_CanMove) ThrowAPopcorn();
+    }
+
+    void ThrowAPopcorn(){
+        m_PopcornShots.GetObject();
+        ThrowerAim.StopAimForThrow();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.CompareTag("Bad")){
+            Death();
+            FallingPopcornManager.Instance.EndMinigame();
+        }
+    }
+
+    void Death(){
+        gameObject.SetActive(false);
+        m_CanMove = false;
+    }
 }
