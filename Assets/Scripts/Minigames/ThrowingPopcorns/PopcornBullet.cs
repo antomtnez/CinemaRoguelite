6e8464b413f7136027517af2bd7c13b6f6214ec2
Anto Martínez <antomartinezdev@gmail.com>
2024-04-17 12:36:50 +0200

    I create and develop popcorn bullet for throwing popcorns mingame

---
+++ Assets/Scripts/Minigames/ThrowingPopcorns/PopcornBullet.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PopcornBullet : MonoBehaviour{
+    [SerializeField] float Speed;
+    private Vector2 m_Direction = Vector2.down;
+
+    void OnEnable(){
+        transform.position = FindObjectOfType<ThrowerPlayerBehaviour>().transform.position;
+        CalculateDirection();
+    }
+
+    void Update(){
+        Move();
+    }
+
+    void Move(){
+        transform.Translate(m_Direction.normalized * Speed * Time.deltaTime);
+        if(IsOutOfBounds()) gameObject.SetActive(false);
+    }
+
+    void CalculateDirection(){
+        Vector2 targetPoint = FindObjectOfType<ThrowerAim>().transform.position;
+        m_Direction = targetPoint - (Vector2)transform.position;
+    }
+
+    bool IsOutOfBounds(){
+        Vector2 screenPosition = Camera.main.WorldToViewportPoint(transform.position);
+        return screenPosition.x > 1 || screenPosition.y < 0;
+    }
+}
