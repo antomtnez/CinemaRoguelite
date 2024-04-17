724513bc56e07570afd860528c36a288f2f4f93c
Anto Martínez <antomartinezdev@gmail.com>
2024-04-17 13:40:28 +0200

    I develop a item scriptable object to manage game items

---
+++ Assets/Scripts/Cinema/Item.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    public string ID;
+    public string Name;
+    public string Description;
+    public int Amount;
+    public Sprite Icon;
+}
