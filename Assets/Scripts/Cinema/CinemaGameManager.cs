f8dcc69259370f85a3db17e4ac101da657770259
Anto Martínez <antomartinezdev@gmail.com>
2024-04-17 13:40:08 +0200

    I develop a game manager for cinema hall scene

---
+++ Assets/Scripts/Cinema/CinemaGameManager.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinemaGameManager : MonoBehaviour{
+    [SerializeField] int Lifes;
+    [SerializeField] List<Item> GameItems;
+    
+
+}
